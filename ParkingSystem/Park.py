9f3bae7bd75aa07c92daf4d163338fe0f2e526c8
omprakashnd <omprakashnd94@gmail.com>
2018-01-04 20:02:44 +0530

    Parking System

--- ParkingSystem/Park.py
+++ ParkingSystem/Park.py
@@ -9,31 +9,31 @@ class Floor:
     def set_slot(self, types, count):
         self.capacity[types] = count
 
-    def park(self, vehicle):
+    def park(self, vehicle):    
         self.capacity[vehicle.vehicle_type] -= 1
         self.parked_vehicle[vehicle.vehicle_number] = vehicle
-        return self.parked_vehicle
+        return vehicle.vehicle_number
     
     def unpark(self, vehicle):
         self.capacity[vehicle.vehicle_type] += 1
         del self.parked_vehicle[vehicle.vehicle_number]
-        return self.parked_vehicle
+        return vehicle.vehicle_number
     
         
 floor1 = Floor("floor1")
-floor1.set_slot("Car", 0)
-floor1.set_slot("Bike", 0)
-floor1.set_slot("Van", 0)
-floor1.set_slot("Bus", 0)
+floor1.set_slot("Car", 10)
+floor1.set_slot("Bike", 10)
+floor1.set_slot("Van", 10)
+floor1.set_slot("Bus", 10)
 
 floor2 = Floor("floor2")
-floor2.set_slot("Bike", 0)
-floor2.set_slot("Van", 0)
-floor2.set_slot("Car", 1)
+floor2.set_slot("Bike",10)
+floor2.set_slot("Van", 5)
+floor2.set_slot("Car", 10)
 
 floor3 = Floor("floor3")
-floor3.set_slot("Car", 0)
-floor3.set_slot("Bike", 0)
+floor3.set_slot("Car", 5)
+floor3.set_slot("Bike", 10)
 
 
 class Vehicles:
@@ -45,6 +45,7 @@ class Vehicles:
 
 
 class ParkingController():
+    
     
     def __init__(self):
         self.list = []
@@ -58,15 +59,16 @@ class ParkingController():
             floor = self.list[index]
             if vehicle.vehicle_type in floor.capacity:
                 if floor.capacity[vehicle.vehicle_type] > 0:
+                    space = "Parked"
                     issuccess = True
                     break
                 else:
-                    nospace = "No free space"
+                    space = "No free space"
                     issuccess = False
             else:
                 not_allowed = "This type of vehicle not allowed"
                 issuccess = False
-        return floor, issuccess
+        return floor, space, issuccess
 
     def check_vehicle(self, vehicle):
         issuccess = True
@@ -74,19 +76,21 @@ class ParkingController():
             floor = self.list[index]
             if vehicle.vehicle_number in floor.parked_vehicle:
                 issuccess = True
+                vehicle_detail = "Vehicle Available"
                 break
             else:
-                no_vehicle = "No vehicle"
+                vehicle_detail = "No vehicle"
                 issuccess = False
-        return floor, issuccess
+        return floor, vehicle_detail, issuccess
 
     def vehicle_info(self):
         vehicle_type = str(raw_input("Enter vehicle type:"))
         vehicle_number = int(raw_input("Enter vehicle number:"))
         return vehicle_type, vehicle_number
-
-    def display(self, floor):
+    
+    def display(self, floor, info):
         print "\tFloor:",floor.floor_no
+        print "\t", info
         print "\tParked vehicles:%r" %floor.parked_vehicle.keys()
         
 
@@ -100,22 +104,22 @@ while success:
 
     choice = int(raw_input(""" \n1.Park\n 2.Unpark\n 3.Exit\n Enter your choice:"""))    
     if choice == 1:
-        vehicle_type, vehicle_number = build.vehicle_info()
-        vehicle = Vehicles(vehicle_type, vehicle_number)
-        floor, issuccess = build.check_space(vehicle)
+        v_type, v_number = build.vehicle_info()
+        vehicle = Vehicles(v_type, v_number)
+        floor, space, issuccess = build.check_space(vehicle)
         if issuccess:
             floor.park(vehicle)
-            build.display(floor)
+            build.display(floor, space)
         else:
             print "No space / Vehicle not allowed"
         
     elif choice == 2:
         vehicle_type, vehicle_number = build.vehicle_info()
         vehicle = Vehicles(vehicle_type, vehicle_number)
-        floor, issuccess = build.check_vehicle(vehicle)
+        floor, vehicle_detail, issuccess = build.check_vehicle(vehicle)
         if issuccess:
-            floor = floor.unpark(vehicle)
-            build.display(floor)
+            floor.unpark(vehicle)
+            build.display(floor, vehicle_detail)
         else:
             print "Not parked"
 
