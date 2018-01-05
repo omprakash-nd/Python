874ca1b27318b6622dded7f64c616e36ef2802b4
omprakashnd <omprakashnd94@gmail.com>
2018-01-05 15:23:48 +0530

    Unit test

--- ParkingSystem/test.py
+++ ParkingSystem/test.py
@@ -7,30 +7,30 @@ class TestParkApplication(unittest.TestCase):
 
     def test_floor(self):
         self.test_floor = Floor("floor2")
-        self.assertEqual((self.test_floor.floor_no), "floor1")
+        self.assertEqual((self.test_floor.floor_no), "floor2")
    
     def test_park(self):
         self.test_floor = Floor("floor1")
         self.test_floor.set_slot("Car", 1)
-        self.test_vehicle = Vehicles("Car", 1234)
+        self.test_vehicle = Vehicle("Car", 1234)
         self.assertEqual(self.test_floor.park(self.test_vehicle), 1234)
 
     def test_unpark(self):
         self.test_floor = Floor("floor1")
         self.test_floor.set_slot("Car", 1)
-        self.test_vehicle = Vehicles("Car", 1234)
+        self.test_vehicle = Vehicle("Car", 1234)
         self.assertEqual(self.test_floor.park(self.test_vehicle), 1234)
 
     def test_unpark(self):
         self.test_floor = Floor("floor1")
         self.test_floor.set_slot("Car", 1)
-        self.test_vehicle = Vehicles("Car", 3555)   
-        self.assertEqual(self.test_floor.park(self.test_vehicle), 1234)
+        self.test_vehicle = Vehicle("Car", 3555)   
+        self.assertNotEqual(self.test_floor.park(self.test_vehicle), 1234)
         
     def test_check_available_space(self):
         self.test_floor = Floor("floor1")
         self.test_floor.set_slot("Car", 1)
-        self.test_vehicle = Vehicles("Car", 1234)
+        self.test_vehicle = Vehicle("Car", 1234)
         self.test_parking_controller = ParkingController()
         self.test_parking_controller.__add__(self.test_floor)
         self.assertEqual(self.test_parking_controller.check_space(self.test_vehicle)[1], "Space Available")
@@ -38,7 +38,7 @@ class TestParkApplication(unittest.TestCase):
     def test_check_filled_space(self):
         self.test_floor = Floor("floor1")
         self.test_floor.set_slot("Car", 0)
-        self.test_vehicle = Vehicles("Car", 1234)
+        self.test_vehicle = Vehicle("Car", 1234)
         self.test_parking_controller = ParkingController()
         self.test_parking_controller.__add__(self.test_floor)
         self.assertEqual(self.test_parking_controller.check_space(self.test_vehicle)[1], "No free space")
@@ -46,7 +46,7 @@ class TestParkApplication(unittest.TestCase):
     def test_check_vehicle(self):
         self.test_floor = Floor("floor1")
         self.test_floor.set_slot("Car", 1)
-        self.test_vehicle = Vehicles("Van", 1234)
+        self.test_vehicle = Vehicle("Van", 1234)
         self.test_parking_controller = ParkingController()
         self.test_parking_controller.__add__(self.test_floor)
         self.assertEqual(self.test_parking_controller.check_vehicle(self.test_vehicle)[1], "No vehicle")
