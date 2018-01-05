7ad37cf3f7484a84517d7c221d01bd88239603cb
omprakashnd <omprakashnd94@gmail.com>
2018-01-05 11:30:58 +0530

    Unittest

--- ParkingSystem/test.py
+++ ParkingSystem/test.py
@@ -1,35 +1,57 @@
 import unittest
-from Park import Floor, Vehicles, ParkingController
+from Floor import Floor
+from Vehicle import Vehicles
+from ParkController import ParkingController
 
 class TestParkApplication(unittest.TestCase):
 
-
-    def setUp(self):
+    def test_floor(self):
+        self.test_floor = Floor("floor2")
+        self.assertEqual((self.test_floor.floor_no), "floor1")
+   
+    def test_park(self):
         self.test_floor = Floor("floor1")
         self.test_floor.set_slot("Car", 1)
-        self.test_floor.set_slot("Van", 0)
-        self.test_floor.set_slot("Bus", 1)
         self.test_vehicle = Vehicles("Car", 1234)
-        self.test_wrong_vehicle = Vehicles("Van", 8343)
-        self.test_parking_controller = ParkingController()
-        self.test_parking_controller.__add__(self.test_floor)
-
-    def test_park(self):
         self.assertEqual(self.test_floor.park(self.test_vehicle), 1234)
 
     def test_unpark(self):
+        self.test_floor = Floor("floor1")
+        self.test_floor.set_slot("Car", 1)
+        self.test_vehicle = Vehicles("Car", 1234)
         self.assertEqual(self.test_floor.park(self.test_vehicle), 1234)
 
+    def test_unpark(self):
+        self.test_floor = Floor("floor1")
+        self.test_floor.set_slot("Car", 1)
+        self.test_vehicle = Vehicles("Car", 3555)   
+        self.assertEqual(self.test_floor.park(self.test_vehicle), 1234)
+        
     def test_check_available_space(self):
-        self.assertEqual(self.test_parking_controller.check_space(self.test_vehicle)[1], "Parked")
+        self.test_floor = Floor("floor1")
+        self.test_floor.set_slot("Car", 1)
+        self.test_vehicle = Vehicles("Car", 1234)
+        self.test_parking_controller = ParkingController()
+        self.test_parking_controller.__add__(self.test_floor)
+        self.assertEqual(self.test_parking_controller.check_space(self.test_vehicle)[1], "Space Available")
 
     def test_check_filled_space(self):
-        self.assertEqual(self.test_parking_controller.check_space(self.test_wrong_vehicle)[1], "No free space")
+        self.test_floor = Floor("floor1")
+        self.test_floor.set_slot("Car", 0)
+        self.test_vehicle = Vehicles("Car", 1234)
+        self.test_parking_controller = ParkingController()
+        self.test_parking_controller.__add__(self.test_floor)
+        self.assertEqual(self.test_parking_controller.check_space(self.test_vehicle)[1], "No free space")
 
     def test_check_vehicle(self):
+        self.test_floor = Floor("floor1")
+        self.test_floor.set_slot("Car", 1)
+        self.test_vehicle = Vehicles("Van", 1234)
+        self.test_parking_controller = ParkingController()
+        self.test_parking_controller.__add__(self.test_floor)
         self.assertEqual(self.test_parking_controller.check_vehicle(self.test_vehicle)[1], "No vehicle")
+
          
-    
 if __name__ == '__main__':
     unittest.main()
 
